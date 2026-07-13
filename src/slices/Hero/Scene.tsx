c93d9a682b94fd63eac50e1e07e4402713fe05a7
Redi Balla <rediballa1@gmail.com>
2026-07-13 18:12:37 +0200

    increase HELL hero can scale

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -14,8 +14,8 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 type Props = {};
 
-const HERO_SCALE = 0.72;
-const PRODUCT_CLUSTER_SCALE = 0.66;
+const HERO_SCALE = 0.82;
+const PRODUCT_CLUSTER_SCALE = 0.76;
 
 export default function Scene({}: Props) {
   const isReady = useStore((state) => state.isReady);
